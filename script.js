d6d7655d50c24710c332c84cc06d481275c1590f
elloveronika <ello.veronika92@gmail.com>
2021-09-18 23:47:06 -0700

    event listener for bot broken

--- script.js
+++ script.js
@@ -15,35 +15,50 @@ function boxSelect() {
   // this loop will identify which box was selected by grabbing my array which now holds box numbers
   for (let i = 0; i < myArr.length; i++) {
     document.getElementById(myArr[i]).addEventListener("click", function () {
-      playerChoice(), console.log(botChoice(playerChoice()));
+      playerChoice(myArr[i]);
+      botChoice(playerChoice(myArr[i]));
+      botPlacement(botChoice(playerChoice(myArr[i])));
     });
     //this is test
   }
 }
 
-function playerChoice() {
-  let select = document.querySelector(".box");
+function playerChoice(choice) {
+  let select = document.getElementById(choice);
   let playerSelect = document.querySelector("select").value;
   select.innerText = playerSelect;
   //   select.addEventListener("click");
-  if (playerSelect == "O") {
-    this.innerText = "O";
-  } else {
-    this.innerText = "X";
-  }
+  //   if (playerSelect == "O") {
+  //     this.innerText = "O";
+  //   } else {
+  //     this.innerText = "X";
+  //   }
   return playerSelect;
   // await botChoice(playerChoice())
   //maybe somehow call  botchoice  inside this function
 }
 
 function botChoice(playerLetter) {
+  //here we are identifying if box id has a value and if it doesnt then display bot choice letter
   let botLetter = "";
   if (playerLetter == "O") {
-    return "hi im X ";
+    botLetter = " X ";
   } else {
-    return "hello, im o";
+    botLetter = "O";
+  }
+
+  return botLetter;
+}
+function botPlacement(placement) {
+  for (let i = 0; i < myArr.length; i++) {
+    let value = document.getElementById(myArr[i]).innerText;
+    console.log(`this is inside for loop: ${myArr[i]}`);
+    if (value == "") {
+      document.getElementById(myArr[i]).innerText = placement;
+    }
   }
 }
+
 //create or add logic that will identify boxes without value to then fill it in with value of bot choice.. maybe with math.random we select a random box without a value
 
 function checkWin() {
