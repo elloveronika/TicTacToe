4064ebd1b5f8bf0b5a5b88a9985d64fd62e14151
elloveronika <ello.veronika92@gmail.com>
2021-09-19 22:40:39 -0700

    bot choice is now identified

--- script.js
+++ script.js
@@ -8,8 +8,9 @@ for (let i = 0; i < boxSections.length; i++) {
   //we are pushing all the box ids into the empty array
 }
 // console.log(myArr)
-document.querySelector(".box").addEventListener("click", boxSelect);
+document.querySelector(".table").addEventListener("click", boxSelect);
 // here we are adding an event listener to identify a click on a box
+//only first box is being selected,, how do i select all
 
 function boxSelect() {
   // this loop will identify which box was selected by grabbing my array which now holds box numbers
@@ -47,16 +48,21 @@ function botChoice(playerLetter) {
     botLetter = "O";
   }
 
+  console.log(botLetter);
   return botLetter;
 }
 function botPlacement(placement) {
   for (let i = 0; i < myArr.length; i++) {
     let value = document.getElementById(myArr[i]).innerText;
-    console.log(`this is inside for loop: ${myArr[i]}`);
+    //   console.log(`this is inside for loop: ${myArr[i]}`);
     if (value == "") {
+      //   let random = Math.random();
+      //out of the 8 boxes available here how do i get a random box
+      //using math random to give me a number out of the boxes without a value,
       document.getElementById(myArr[i]).innerText = placement;
     }
   }
+  console.log(`this is inside botPlacement ${placement}`);
 }
 
 //create or add logic that will identify boxes without value to then fill it in with value of bot choice.. maybe with math.random we select a random box without a value
